14ae321303c5719258f63e1ff6e1b1b0f84de2df
PeterMattenet <pmattenet@gmail.com>
2024-05-01 14:21:17 -0300

    Refactor names

--- Scripts/Components/Sprite2DComponent.cs
+++ Scripts/Components/Sprite2DComponent.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class SpriteComponent : Sprite2D
+public partial class Sprite2DComponent : Sprite2D
 {
 	[Export]
 	private MovementComponent movementComponent;
