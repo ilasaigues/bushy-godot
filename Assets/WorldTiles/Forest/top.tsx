0a742ba42aa8f4c725942343c7e09e5c6b62d3ef
Tomas <tomasalvesdeazevedo@gmail.com>
2024-06-06 21:33:00 -0300

    mini slope map

---
+++ Assets/WorldTiles/Forest/top.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="top" tilewidth="480" tileheight="270" tilecount="1" columns="1">
+ <image source="../treealt.png" width="480" height="270"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="7" x="91" y="203">
+    <polygon points="0,0 79,0 85,1 112,1 118,0 200,0 206,1 241,1 247,0 291,0 291,-57 324,-57 324,67 -31,67 -31,-68 0,-68"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
